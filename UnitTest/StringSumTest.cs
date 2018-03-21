a4eff611e290487fbd7983b01b50c38689056ed8
ALEXANDER-VAIO\Alexander <aleovs@gmail.com>
2018-03-21 20:06:57 +0300

    Unused paratemers have been deleted from unit tests

--- UnitTest/StringSumTest.cs
+++ UnitTest/StringSumTest.cs
@@ -20,21 +20,22 @@ namespace StringSumTest
         }
 
         [Theory]
-        [InlineData("-1", "-1", "-2", true)]
-        [InlineData("-1", "0", "-1", true)]
-        [InlineData("0", "-1", "-1", true)]
-        [InlineData("0", "0", "0", true)]
-        [InlineData("0", "1", "1", true)]
-        [InlineData("1", "0", "1", true)]
-        [InlineData("1", "2", "3", true)]
-        [InlineData("12345678901234567890", "23232323232323232324", "35578002133557800214", true)]
-        [InlineData(MaxValue, "0", MaxValue, true)]
-        [InlineData("0", MaxValue, MaxValue, true)]
-        [InlineData(MinValue, "0", MinValue, true)]
-        [InlineData("0", MinValue, MinValue, true)]
-        public void GetSum_ValidXAndValidY_ReturnsSum(string x, string y, string sum, bool success)
+        [InlineData("-1", "-1", "-2")]
+        [InlineData("-1", "0", "-1")]
+        [InlineData("0", "-1", "-1")]
+        [InlineData("0", "0", "0")]
+        [InlineData("0", "1", "1")]
+        [InlineData("1", "0", "1")]
+        [InlineData("1", "2", "3")]
+        [InlineData("12345678901234567890", "23232323232323232324", "35578002133557800214")]
+        [InlineData(MaxValue, "0", MaxValue)]
+        [InlineData("0", MaxValue, MaxValue)]
+        [InlineData(MinValue, "0", MinValue)]
+        [InlineData("0", MinValue, MinValue)]
+        public void GetSum_ValidXAndValidY_ReturnsSum(string x, string y, string sum)
         {
             //arrange
+            bool success;
 
             //act
             string result = stringSum.TryGetSum(x, y, out success);
@@ -45,23 +46,24 @@ namespace StringSumTest
         }
 
         [Theory]
-        [InlineData(default(string), default(string), default(string), default(bool))]
-        [InlineData(default(string), "1", default(string), default(bool))]
-        [InlineData("1", default(string), default(string), default(bool))]
-        [InlineData(NotDigit, default(string), default(string), default(bool))]
-        [InlineData(default(string), NotDigit, default(string), default(bool))]
-        [InlineData(NotDigit, NotDigit, default(string), default(bool))]
-        [InlineData("", "", default(string), default(bool))]
-        [InlineData(" ", " ", default(string), default(bool))]
-        [InlineData(NotDigitWithWhiteSpace, "", default(string), default(bool))]
-        [InlineData("", NotDigitWithWhiteSpace, default(string), default(bool))]
-        [InlineData(MaxValue, 1, default(string), default(bool))]
-        [InlineData(1, MaxValue, default(string), default(bool))]
-        [InlineData(MinValue, -1, default(string), default(bool))]
-        [InlineData(-1, MinValue, default(string), default(bool))]
-        public void GetSum_InValidXValidY_ReturnsNull(string x, string y, string sum, bool success)
+        [InlineData(default(string), default(string), default(string))]
+        [InlineData(default(string), "1", default(string))]
+        [InlineData("1", default(string), default(string))]
+        [InlineData(NotDigit, default(string), default(string))]
+        [InlineData(default(string), NotDigit, default(string))]
+        [InlineData(NotDigit, NotDigit, default(string))]
+        [InlineData("", "", default(string))]
+        [InlineData(" ", " ", default(string))]
+        [InlineData(NotDigitWithWhiteSpace, "", default(string))]
+        [InlineData("", NotDigitWithWhiteSpace, default(string))]
+        [InlineData(MaxValue, 1, default(string))]
+        [InlineData(1, MaxValue, default(string))]
+        [InlineData(MinValue, -1, default(string))]
+        [InlineData(-1, MinValue, default(string))]
+        public void GetSum_InValidXValidY_ReturnsNull(string x, string y, string sum)
         {
             //arrange
+            bool success;
 
             //act
             string result = stringSum.TryGetSum(x, y, out success);
